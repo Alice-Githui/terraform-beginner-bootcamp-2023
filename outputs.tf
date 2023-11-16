18092a89633f24e11475dd47084ff5beb1ed8c11
Alice Githui <gnyokabi.alice@gmail.com>
2023-11-16 19:39:13 +0000

    #35 invalidate cloudfront cache when contentversion changes

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "s3_website_endpoint" {
   description = "S3 static website hosting endpoint"
   value= module.terrahouse_aws.website_endpoint
 }
+
+output "cloudfront_url" {
+  description ="cloudfront url to access the website hosted on s3"
+  value = module.terrahouse_aws.cloudfront_url
+}
