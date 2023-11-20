9dabd33b2a328ecdf10d3dbbd1f77928faf13d39
Alice Githui <gnyokabi.alice@gmail.com>
2023-11-20 16:47:59 +0000

    destroy using gitops

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-output "bucket_name" {
-  description="Bucket name for our static website"
-  value=module.terrahouse_aws.bucket_name
-}
+# output "bucket_name" {
+#   description="Bucket name for our static website"
+#   value=module.terrahouse_aws.bucket_name
+# }
 
-output "s3_website_endpoint" {
-  description = "S3 static website hosting endpoint"
-  value= module.terrahouse_aws.website_endpoint
-}
+# output "s3_website_endpoint" {
+#   description = "S3 static website hosting endpoint"
+#   value= module.terrahouse_aws.website_endpoint
+# }
